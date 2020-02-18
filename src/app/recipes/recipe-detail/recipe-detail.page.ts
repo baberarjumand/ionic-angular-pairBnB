330fd1c3e73d12c414cff6c8bf7a4eef8b278dd2
baberarjumand <baber.arjumand@gmail.com>
2020-02-18 11:16:36 +0300

    completed exercise 4.16, injecting ionic controllers

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,7 @@ import { RecipesService } from './../recipes.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,7 +15,9 @@ export class RecipeDetailPage implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute,
               private recipesServce: RecipesService,
-              private router: Router) { }
+              private router: Router,
+              private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -28,8 +31,22 @@ export class RecipeDetailPage implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipesServce.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['/recipes']);
+    this.alertCtrl.create({
+      header: 'Are you sure?',
+      message: 'Do you really want to delete the recipe?',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel'
+      }, {
+        text: 'Delete',
+        handler: () => {
+          this.recipesServce.deleteRecipe(this.loadedRecipe.id);
+          this.router.navigate(['/recipes']);
+        }
+      }]
+    }).then(alertEl => {
+      alertEl.present();
+    });
   }
 
 }
