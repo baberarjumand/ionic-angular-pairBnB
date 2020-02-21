f7cf78b55e1b4775cf988e0bdf07a9670c31c834
baberarjumand <baber.arjumand@gmail.com>
2020-02-21 15:07:12 +0300

    completed exercise 7.9, preparing data and services

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private _places: Place[] = [
+    new Place('p1',
+              'Manhattan Mansion',
+              'In the heart of New York City.',
+              'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
+              149.99
+      ),
+      new Place('p2',
+              "L'Amour Toujours",
+              'A romantic place in Paris!',
+              'https://tophotel.news/wp-content/uploads/2018/06/25hours-francess-1024x512.jpg',
+              189.99
+      ),
+      new Place('p3',
+              'The Foggy Palace',
+              'Not your average city trip!',
+              'https://i.pinimg.com/originals/65/8f/77/658f77b9b527f89922ba996560a3e2b0.jpg',
+              99.99
+      )
+];
+
+  constructor() { }
+
+  getPlaces() {
+    return [...this._places];
+  }
+}
