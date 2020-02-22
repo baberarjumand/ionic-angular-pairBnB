1263fc338fb85754229855b8f97bb27a47e53ff0
baberarjumand <baber.arjumand@gmail.com>
2020-02-22 11:35:55 +0300

    completed exercise 7.20, open close sidedrawer

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Place } from './../place.model';
 import { PlacesService } from './../places.service';
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -11,10 +12,15 @@ export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService,
+              private menuCtrl: MenuController) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.getPlaces();
+  }
+
+  onOpenMenu() {
+    this.menuCtrl.toggle();
   }
 
 }
