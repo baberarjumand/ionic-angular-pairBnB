0c9f8686a151b530696738c4e81130123ef44e7a
baberarjumand <baber.arjumand@gmail.com>
2020-02-25 10:10:55 +0300

    completed exercise 10.19, validating and submitting form

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,43 +1,70 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Place } from 'src/app/places/place.model';
 import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-booking',
   templateUrl: './create-booking.component.html',
-  styleUrls: ['./create-booking.component.scss'],
+  styleUrls: ['./create-booking.component.scss']
 })
 export class CreateBookingComponent implements OnInit {
-
   @Input() selectedPlace: Place;
   @Input() selectedMode: 'select' | 'random';
   startDate: string;
   endDate: string;
+  @ViewChild('f', { static: false }) form: NgForm;
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {
     const availableFrom = new Date(this.selectedPlace.availableFrom);
     const availableTo = new Date(this.selectedPlace.availableTo);
     if (this.selectedMode === 'random') {
-      this.startDate = new Date(availableFrom.getTime() +
-                                  Math.random() *
-                                  (availableTo.getTime() - 7 * 24 * 60 * 60 * 1000 - availableFrom.getTime())
-        ).toISOString();
-      this.endDate = new Date(new Date(this.startDate)
-          .getTime() +
+      this.startDate = new Date(
+        availableFrom.getTime() +
           Math.random() *
-          (new Date(this.startDate).getTime() +
-          6 * 24 * 60 * 60 - new Date(this.startDate).getTime())).toISOString();
+            (availableTo.getTime() -
+              7 * 24 * 60 * 60 * 1000 -
+              availableFrom.getTime())
+      ).toISOString();
+      this.endDate = new Date(
+        new Date(this.startDate).getTime() +
+          Math.random() *
+            (new Date(this.startDate).getTime() +
+              6 * 24 * 60 * 60 -
+              new Date(this.startDate).getTime())
+      ).toISOString();
     }
   }
 
   onBookPlace() {
-    this.modalCtrl.dismiss({ message: 'A dummy message!'}, 'confirm');
+    if (!this.form.valid || !this.datesValid) {
+      return;
+    }
+
+    // this.modalCtrl.dismiss({ message: 'A dummy message!'}, 'confirm');
+    this.modalCtrl.dismiss(
+      {
+        bookingData: {
+          firstName: this.form.value['first-name'],
+          lastName: this.form.value['last-name'],
+          guestNumber: this.form.value['guest-number'],
+          startDate: this.form.value['date-from'],
+          endDate: this.form.value['date-to']
+        }
+      },
+      'confirm'
+    );
   }
 
   onCancel() {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
+  datesValid() {
+    const startDate = new Date(this.form.value['date-from']);
+    const endDate = new Date(this.form.value['date-to']);
+    return endDate > startDate;
+  }
 }
