bbf6c11391b39655fabcf9bc2ec1691ba0f63f7b
baberarjumand <baber.arjumand@gmail.com>
2020-03-01 14:24:09 +0300

    completed exercise 11.12, cancelling bookings

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,7 +1,7 @@
 import { BookingService } from './booking.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Booking } from './booking.model';
-import { IonItemSliding } from '@ionic/angular';
+import { IonItemSliding, LoadingController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,7 +13,10 @@ export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
   private bookingSub: Subscription;
 
-  constructor(private bookingsService: BookingService) {}
+  constructor(
+    private bookingsService: BookingService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
     this.bookingSub = this.bookingsService.getBookings().subscribe(bookings => {
@@ -29,6 +32,11 @@ export class BookingsPage implements OnInit, OnDestroy {
 
   onCancelBooking(bookingId: string, slidingItem: IonItemSliding) {
     slidingItem.close();
-    // cancel booking with bookingId
+    this.loadingCtrl.create({message: 'Cancelling booking...'}).then(loadingEl => {
+      loadingEl.present();
+      this.bookingsService.cancelBooking(bookingId).subscribe(() => {
+        loadingEl.dismiss();
+      });
+    });
   }
 }
