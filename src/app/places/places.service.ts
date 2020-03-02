3fa931de43dbe0859880e8082ebac784e01f233a
baberarjumand <baber.arjumand@gmail.com>
2020-03-02 09:33:53 +0300

    completed exercise 12.7, updating places

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -160,12 +160,12 @@ export class PlacesService {
   }
 
   updatePlace(placeId: string, title: string, description: string) {
+    let updatedPlaces: Place[];
     return this.getPlaces().pipe(
       take(1),
-      delay(1000),
-      tap(places => {
+      switchMap(places => {
         const updatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
-        const updatedPlaces = [...places];
+        updatedPlaces = [...places];
         const oldPlace = updatedPlaces[updatedPlaceIndex];
         updatedPlaces[updatedPlaceIndex] = new Place(
           oldPlace.id,
@@ -177,6 +177,12 @@ export class PlacesService {
           oldPlace.availableTo,
           oldPlace.userId
         );
+        return this.httpClient.put(
+          `https://ionic-angular-udemy-by-max.firebaseio.com/offered-places/${placeId}.json`,
+          { ...updatedPlaces[updatedPlaceIndex], id: null }
+        );
+      }),
+      tap(() => {
         this._places.next(updatedPlaces);
       })
     );
