2559739f4e65161a433198f6cd9c4fe2b8d6d2b3
baberarjumand <baber.arjumand@gmail.com>
2020-03-08 11:10:03 +0300

    completed exercise 14.12, adding filepicker fallback

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -5,7 +5,14 @@ import {
   CameraSource,
   CameraResultType
 } from '@capacitor/core';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-image-picker',
@@ -15,6 +22,10 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class ImagePickerComponent implements OnInit {
   selectedImage: string;
   @Output() imagePick = new EventEmitter<string>();
+  useFilePicker = false;
+  @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<
+    HTMLInputElement
+  >;
 
   constructor(private platform: Platform) {}
 
@@ -24,10 +35,17 @@ export class ImagePickerComponent implements OnInit {
     console.log('iOS: ', this.platform.is('ios'));
     console.log('Android: ', this.platform.is('android'));
     console.log('Desktop: ', this.platform.is('desktop'));
+    if (
+      (this.platform.is('mobile') && !this.platform.is('hybrid')) ||
+      this.platform.is('desktop')
+    ) {
+      this.useFilePicker = true;
+    }
   }
 
   onPickImage() {
-    if (!Capacitor.isPluginAvailable('Camera')) {
+    if (!Capacitor.isPluginAvailable('Camera') || this.useFilePicker) {
+      this.filePickerRef.nativeElement.click();
       return;
     }
     Plugins.Camera.getPhoto({
@@ -49,5 +67,9 @@ export class ImagePickerComponent implements OnInit {
         console.log(error);
         return false;
       });
+  }
+
+  onFileChosen(event: Event) {
+    console.log(event);
   }
 }
