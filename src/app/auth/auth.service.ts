2ab71fd87bf23787750e726bebb7686e2f16d76d
baberarjumand <baber.arjumand@gmail.com>
2020-03-12 13:26:32 +0300

    completed exercise 15.14, adding autologin, fixed commit

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { map, tap } from 'rxjs/operators';
 import { User } from './user.model';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, from } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
@@ -103,23 +103,61 @@ export class AuthService {
     this.storeAuthData(
       userData.localId,
       userData.idToken,
-      expirationTime.toISOString()
+      expirationTime.toISOString(),
+      userData.email
     );
   }
 
   private storeAuthData(
     userId: string,
     token: string,
-    tokenExpirationDate: string
+    tokenExpirationDate: string,
+    email: string
   ) {
     const data = JSON.stringify({
       userId,
       token,
-      tokenExpirationDate
+      tokenExpirationDate,
+      email
     });
     Plugins.Storage.set({
       key: 'authData',
       value: data
     });
+  }
+
+  autoLogin() {
+    return from(Plugins.Storage.get({ key: 'authData' })).pipe(
+      map(storedData => {
+        if (!storedData || !storedData.value) {
+          return null;
+        }
+        const parsedData = JSON.parse(storedData.value) as {
+          userId: string;
+          token: string;
+          tokenExpirationDate: string;
+          email: string;
+        };
+        const expirationTime = new Date(parsedData.tokenExpirationDate);
+        if (expirationTime <= new Date()) {
+          return null;
+        }
+        const user = new User(
+          parsedData.userId,
+          parsedData.email,
+          parsedData.token,
+          expirationTime
+        );
+        return user;
+      }),
+      tap(user => {
+        if (user) {
+          this._user.next(user);
+        }
+      }),
+      map(user => {
+        return !!user;
+      })
+    );
   }
 }
