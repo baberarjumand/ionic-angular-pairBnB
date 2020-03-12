da4fa8c2eeee0ee91c725c25f93e9aeae78c5b39
baberarjumand <baber.arjumand@gmail.com>
2020-03-12 16:41:37 +0300

    completed exercise 15.21, check auth state when app resumes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Subscription } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Platform } from '@ionic/angular';
-import { Plugins, Capacitor } from '@capacitor/core';
+import { Plugins, Capacitor, AppState } from '@capacitor/core';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { AuthService } from './auth/auth.service';
 
@@ -14,21 +15,6 @@ import { AuthService } from './auth/auth.service';
 export class AppComponent implements OnInit, OnDestroy {
   private authSub: Subscription;
   private previousAuthState = false;
-
-  ngOnDestroy(): void {
-    if (this.authSub) {
-      this.authSub.unsubscribe();
-    }
-  }
-
-  ngOnInit(): void {
-    this.authSub = this.authService.userIsAuthenticated.subscribe(isAuth => {
-      if (!isAuth && this.previousAuthState !== isAuth) {
-        this.router.navigateByUrl('/auth');
-      }
-      this.previousAuthState = isAuth;
-    });
-  }
 
   constructor(
     private platform: Platform,
@@ -46,7 +32,40 @@ export class AppComponent implements OnInit, OnDestroy {
     });
   }
 
+  ngOnInit() {
+    this.authSub = this.authService.userIsAuthenticated.subscribe(isAuth => {
+      if (!isAuth && this.previousAuthState !== isAuth) {
+        this.router.navigateByUrl('/auth');
+      }
+      this.previousAuthState = isAuth;
+    });
+    Plugins.App.addListener(
+      'appStateChange',
+      this.checkAuthOnResume.bind(this)
+    );
+  }
+
   onLogout() {
     this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    if (this.authSub) {
+      this.authSub.unsubscribe();
+    }
+    // Plugins.App.removeListener('appStateChange', this.checkAuthOnResume);
+  }
+
+  private checkAuthOnResume(state: AppState) {
+    if (state.isActive) {
+      this.authService
+        .autoLogin()
+        .pipe(take(1))
+        .subscribe(success => {
+          if (!success) {
+            this.onLogout();
+          }
+        });
+    }
   }
 }
